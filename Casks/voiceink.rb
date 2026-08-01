448f06e64b8bb540a1aba318da7f62cdf504ab95
edenliu <hxfdarling@hotmail.com>
2026-08-02 01:02:50 +0800

    Add VoiceInk cask

---
+++ Casks/voiceink.rb
@@ -0,0 +1,19 @@
+cask "voiceink" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/bifrost-proxy/VoiceInk/releases/latest/download/VoiceInk.zip"
+  name "VoiceInk"
+  desc "Privacy-focused voice-to-text app"
+  homepage "https://github.com/bifrost-proxy/VoiceInk"
+
+  depends_on macos: :sonoma
+
+  app "VoiceInk.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.prakashjoshipax.VoiceInk",
+    "~/Library/Caches/com.prakashjoshipax.VoiceInk",
+    "~/Library/Preferences/com.prakashjoshipax.VoiceInk.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# Homebrew VoiceInk
+
+Homebrew tap for the free [Bifrost Proxy VoiceInk fork](https://github.com/bifrost-proxy/VoiceInk).
+
+Install with the fully qualified Cask name:
+
+```shell
+brew install --cask bifrost-proxy/voiceink/voiceink
+```
+
+Or use the equivalent two-step form:
+
+```shell
+brew tap bifrost-proxy/voiceink
+brew install --cask voiceink
+```
+
+Upgrade to the latest GitHub Release:
+
+```shell
+brew upgrade --cask --greedy voiceink
+```
